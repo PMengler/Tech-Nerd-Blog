511b7e660ac9c517cf77823ce3c9fc66948a20a5
PMengler <114950259+PMengler@users.noreply.github.com>
2023-02-08 19:51:05 -0800

    updated routes

--- Main/controllers/dashboard-routes.js
+++ Main/controllers/dashboard-routes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
-router.use('/dashboard', require('./dashboard'));
+// router.use('/dashboard', require('./dashboard'));
 
 router.get('/', (req, res) => {
   res.render('/dashboard');
--- Main/controllers/home-routes.js
+++ Main/controllers/home-routes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
-router.use('/home', require('./home'));
+// router.use('/home', require('./home'));
 
 router.get('/', (req, res) => {
   res.render('index');
