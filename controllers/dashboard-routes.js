6c23c3440562db1f5cb3e14525abff788efb843d
PMengler <114950259+PMengler@users.noreply.github.com>
2023-02-11 13:52:03 -0800

    handlebars are coming along

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,7 +2,11 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-  res.render('/dashboard');
+  if (req.session.loggedIn) {
+    res.render('');
+  } else {
+    res.redirect('/login');
+  }
 });
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const apiRoutes = require('./api');
+const { User, Post, Comment } = require('../models');
 
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
@@ -7,6 +8,31 @@ router.get('/login', (req, res) => {
     return;
   }
   res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('signup');
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const dbPostData = await Post.findAll({});
+
+    const posts = dbPostData.map((user) => user.get({ plain: true }));
+    res.render('all-posts', {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+
+    console.log(posts);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
