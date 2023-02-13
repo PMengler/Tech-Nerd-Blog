23d39aebe4afaacdcb93ea3b7a32a2ccb07c3a7e
PMengler <114950259+PMengler@users.noreply.github.com>
2023-02-13 11:02:05 -0800

    bug fixing

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 const hbs = exphbs.create({ helpers });
 
